87a7996f05af5800d2ae0c8ca56cdd0fe483cdb9
Yuvraj Singh <mail.yuvrajsingh23@gmail.com>
2024-08-13 22:30:23 +0530

    created: database

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,10 +27,10 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => '',
-        'password'     => '',
-        'database'     => '',
-        'DBDriver'     => 'MySQLi',
+        'username'     => 'root',
+        'password'     => 'root',
+        'database'     => 'project_database',
+        'DBDriver'     => 'Postgre',
         'DBPrefix'     => '',
         'pConnect'     => false,
         'DBDebug'      => true,
@@ -41,7 +41,7 @@ class Database extends Config
         'compress'     => false,
         'strictOn'     => false,
         'failover'     => [],
-        'port'         => 3306,
+        'port'         => 5432,
         'numberNative' => false,
         'dateFormat'   => [
             'date'     => 'Y-m-d',
